512f455f8207a72b7f7e78a248d71ef7642cdbf5
Singh <Adarsh.Singh@elektrobit.com>
2023-12-18 22:03:49 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        bankApplication b1 = new bankApplication(0); // 0-savings, 1-current, 2- demat, 4-FD
+        b1.deposit(5000);
+        System.out.println(b1.getBalance());
+        b1.withdraw(1000);
+
+        System.out.println(b1.getBalance());
+
+
+        b1.setAcType(0); // 0-savings, 1-current, 2- demat, 4-FD
+        System.out.println(b1.getIntRate());
+
+    }
+}
